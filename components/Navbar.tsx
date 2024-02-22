b4893d53cfdcc77dded2195689e24cf44bb5766b
seba-sanchis <seba.sanchis@gmail.com>
2024-02-22 12:43:56 -0300

    added pathname to indicate the active page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
-import { features } from "@/constants";
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { features } from "@/constants";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex justify-center sticky top-0 bg-[hsla(0,0%,100%,.8)] dark:bg-[rgba(0,0,0,.8)] shadow-[inset_0_-1px_0_0_var(--accents-2)] saturate-200 backdrop-blur-sm z-50">
       <div className="flex items-center gap-8 max-w-screen-xl w-full h-16 px-14">
@@ -10,7 +16,11 @@ export default function Navbar() {
           {features.map((feature) => (
             <li
               key={feature.name}
-              className="text-sm text-[--accents-5] hover:text-[--foreground]"
+              className={`text-sm hover:text-[--foreground] ${
+                pathname === feature.url
+                  ? "text-[--foreground]"
+                  : "text-[--accents-5]"
+              }`}
             >
               <Link href={feature.url}>{feature.name}</Link>
             </li>
